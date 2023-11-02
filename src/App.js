0a9320429c28c5a142059e9ad47cdab9be83657c
Ahmed Kotb <akotb621@gmail.com>
2023-11-02 14:07:19 +0300

    draw.io

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
  
 import { Container } from 'react-bootstrap';
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
-    <div>
- 
-        <h1>bootstrap</h1>
-        <p>lorem ahmed djsf</p>
- 
-    </div>
+    <NavBar />
   );
 }
 
